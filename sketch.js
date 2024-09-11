5aa0d6c50a582fd7df30c0362038c1991385052f
Gloria Saija <blackiedoescomics@gmail.com>
2024-09-11 10:51:00 +0200

    stage model added

--- sketch.js
+++ sketch.js
@@ -5,13 +5,15 @@ function preload() {
   img = loadImage('./assets/tiles.jpg')
   steve = loadImage('./assets/chark.jpg')
   enemy = loadImage('./assets/enemy.png')
+  enemy1 = loadImage('./assets/enemy1.png')
   char = loadModel('./assets/char.obj')
+  stage = loadModel('./assets/stage.obj')
 }
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   x = 0;
-  y = -90;
+  y = 0;
   z = 0;
   enemyDist = -300
   charR = radians(0);
@@ -24,35 +26,20 @@ function draw() {
   capture.hide()
   noStroke()
 
-
-
-
   //scene
   push()
-  rotateY(radians(-y * 4))
+  rotateY(radians(-y * 6))
   noStroke()
-  //floor
+  //stage
   push()
-  translate(100, 2, -100)
-  textureMode(NORMAL)
-  textureWrap(REPEAT)
+  rotateY(radians(180))
+  scale(1, .8, 1)
   texture(img)
-  box(width / 2, 2, width / 2)
-  pop()
-  //walls
-  push()
-  texture(img)
-  translate(200, -48, -200)
-  box(200, 100, 200)
-  pop()
-  push()
-  texture(img)
-  translate(-200, -48, -200)
-  box(2, 100, 200)
+  model(stage)
   pop()
   //skybox
   texture(sky)
-  box(width * 10, width * 10, width * 10)
+  box(width * 6, width * 6, width * 6)
   pop()
 
   //character
@@ -64,9 +51,9 @@ function draw() {
   pop()
 
 
-  //enemies
+  //ENEMIES
   push()
-  rotateY(radians(-y * 4))
+  rotateY(radians(-y * 6))
 
   //enemy north
   push()
@@ -77,30 +64,14 @@ function draw() {
 
   //enemy east
   push()
-  texture(enemy)
-  rotateY(radians(-90))
-  fill(255, 100, 0)
+  texture(enemy1)
+  rotateY(radians(-90)) //this changes which tunnel the enemy comes from
   translate(0, -30, enemyDist)
   box(90, 60, 20)
   pop()
 
   pop()
-  // //enemy south
-  // push()
-  // rotateY(radians(-y * 4))
-  // texture(enemy)
-  // translate(0, -30, 300)
-  // box(90, 60, 20)
-  // pop()
 
-  // //enemy west
-  // push()
-  // rotateY(radians(-y * 4))
-  // texture(enemy)
-  // fill(255, 100, 0)
-  // translate(-300, -30, 0)
-  // box(90, 60, 20)
-  // pop()
 
   //webcam
   // push()
@@ -111,15 +82,18 @@ function draw() {
   // pop()
 
   //gizmo
+  // push()
+  // rotateY(radians(-y * 10))
   // stroke(255, 0, 0)
   // line(0, 0, 0, 800, 0, 0)
   // stroke(0, 255, 0)
   // line(0, 0, 0, 0, -800, 0)
   // stroke(0, 0, 255)
   // line(0, 0, 0, 0, 0, -800)
+  // pop()
 
   //attack beam
-  stroke(255, 0, 0)
+  stroke(255, 255, 0)
   strokeWeight(4)
   line(0, -20, 0, 0, -25, -800)
 
@@ -127,15 +101,16 @@ function draw() {
   //player movements
   if (keyIsPressed) {
 
-    if (keyCode === LEFT_ARROW) {
+    if (keyCode === LEFT_ARROW || key === 'a') {
       y++
       charR = radians(10)
-    } else if (keyCode === RIGHT_ARROW) {
+    } else if (keyCode === RIGHT_ARROW || key === 'd') {
       y--
       charR = radians(-10)
     }
-  } else {
-    charR = radians(0);
+    else {
+      charR = radians(0);
+    }
   }
 
   //game camera
@@ -143,11 +118,8 @@ function draw() {
 
   //editing camera
   camera(10, -50, 100, 20, -40, 0)
-  strokeWeight(1)
-  debugMode(AXES)
 
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
